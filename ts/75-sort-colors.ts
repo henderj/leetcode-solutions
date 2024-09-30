81e23756e923331ce534b2458b94efcdb1ae0632
Josh Hendershot <hendershotjoshr@gmail.com>
2024-09-30 10:35:05 -0600

    solved 75 using one-pass

--- ts/75-sort-colors.ts
+++ ts/75-sort-colors.ts
@@ -2,24 +2,30 @@
  Do not return anything, modify nums in-place instead.
  */
 function sortColors(nums: number[]): void {
-  const count0 = nums.reduce((count, curr) => {
-    if (curr != 0) return count;
-    return count + 1
-  }, 0)
-  const count1 = nums.reduce((count, curr) => {
-    if (curr != 1) return count;
-    return count + 1
-  }, 0)
+  let next0 = -1
+  let next1 = -1
+
+  function swap(a: number, b: number) {
+    if (a == b) return;
+    const temp = nums[a]
+    nums[a] = nums[b]
+    nums[b] = temp
+  }
 
   for (let i = 0; i < nums.length; i++) {
-    if (i < count0) {
-      nums[i] = 0
-      continue
+    if (nums[i] == 0) {
+        swap(++next0, i)
+        if (next0 > next1) {
+          next1 = next0
+        }
     }
-    if (i < count0 + count1) {
-      nums[i] = 1
-      continue
+    if (nums[i] == 1) {
+      swap(++next1, i)
     }
-    nums[i] = 2
   }
 };
+
+// const a = [2, 0, 2, 1, 1, 0]
+const a = [2, 0, 1]
+sortColors(a)
+console.log(a)
